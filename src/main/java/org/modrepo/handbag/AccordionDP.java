c1c6a1972020cf889e8c6a5c84752d349698a7b1
Richard Rodgers <richard.homerun@gmail.com>
2023-07-02 16:35:00 -0400

    WIP for large refactor

--- src/main/java/org/modrepo/handbag/AccordionDP.java
+++ src/main/java/org/modrepo/handbag/AccordionDP.java
@@ -1,8 +1,16 @@
+/**
+ * Copyright 2023 Richard Rodgers
+ * SPDX-Licence-Identifier: Apache-2.0
+ */
 package org.modrepo.handbag;
 
 import javafx.beans.DefaultProperty;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.TitledPane;
+
+/*
+ * Trivial extension to leverage DefaultProperty in FXML
+ */
 
 @DefaultProperty(value="panes")
 public class AccordionDP extends Accordion {
---
+++ src/main/java/org/modrepo/handbag/model/JobSpec.java
@@ -0,0 +1,5 @@
+package org.modrepo.handbag.model;
+
+public class JobSpec {
+    
+}
